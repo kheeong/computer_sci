f5626136f089fb763c2b241aff2bf26d15c66019
Ng <Oscar.ng@publicgold.com.my>
2020-10-20 13:30:54 +0800

    v4

--- main_2.py
+++ main_2.py
@@ -9,18 +9,22 @@ def input_Baguettes_size():
     print("Input your Baguettes Size by typing 15 or 30")
     Baguettes_size = int(input())
     while (Baguettes_size != 15 and Baguettes_size != 30):
-        print("Baguettes Size not valid, please input again")
-        Baguettes_size = int(input())
+    # while Baguettes_size in [15, 30]:
+            print("Baguettes Size not valid, please input again")
+            Baguettes_size = int(input())
     return Baguettes_size
 
 def input_Baguettes_bread():
     print("Baguettes bread available in white,brown or seeded")
     print("Input your Baguettes bread by typing white,brown or seeded")
     Baguettes_bread = input()
-
-    while(Baguettes_bread != "white" and Baguettes_bread != "brown" and Baguettes_bread != "seeded"):
-        print("Baguettes bread not valid, please input again")
-        Baguettes_bread = input()
+    Baguettes_bread_valid = False
+    while(Baguettes_bread_valid == False):
+        if(Baguettes_bread == "white" or Baguettes_bread == "brown" or Baguettes_bread == "seeded"):
+            Baguettes_bread_valid = True
+        else:
+            print("Baguettes bread not valid, please input again")
+            Baguettes_bread = input()
     return Baguettes_bread
 
 def input_filling():
@@ -28,23 +32,23 @@ def input_filling():
     print("Please enter the filling of your choice")
     Filling_list = ["Beef","Chicken","Cheese","Egg","Tuna","Turkey"]
     Filling = input()
-    filling_valid = False
-    while(True):
-        for item in Filling_list:
-            if(item == Filling):
-                filling_valid = True
-        if(filling_valid == True):
-            break
+
+    while True:
+        for f in Filling_list:
+            if Filling == f:
+                break
         print("Filling not valid,please enter again")
         Filling = input()
     return Filling
 
+
 def input_salad():
     print("Please enter number of Salad you wanted to add")
     number_of_salad = int(input())
-    while(True):
+    number_of_salad_valid = False
+    while(number_of_salad_valid == False):
         if(number_of_salad <= 3):
-            break
+            number_of_salad_valid = True
         else:
             print("number of salad is more than 3, maxinum of 3 is allow, please enter again")
             number_of_salad = int(input())
@@ -54,18 +58,38 @@ def input_salad():
     for i in range(number_of_salad):
         print("Please enter your salad")
         salad_input = input()
-        salad_valid = False
-        while(True):
+        salad_input_valid = False
+        while(salad_input_valid == False):
             for item in Salad_list:
                 if(salad_input == item):
-                    salad_valid = True
-            if(salad_valid == True):
-                break
-            print("Salad not valid,please enter again")
-            salad_input = input()
+                    salad_input_valid = True
+            if(salad_input_valid == False):
+                print("Salad not valid,please enter again")
+                salad_input = input()
         salad.append(salad_input)
     return salad
 
+def alter():
+    alter_order_valid = False
+    while(alter_order_valid == False):
+        print("Please type which order you wanted to alter")
+        print("Type Baguettes Size,Baguettes Bread,Filling or Salads")
+        alter_order = input()
+        if(alter_order == "Baguettes Size"):
+            Gobal_Baguettes_size = input_Baguettes_size()
+            alter_order_valid = True
+        elif(alter_order == "Baguettes Bread"):
+            Gobal_Baguettes_bread = input_Baguettes_bread()
+            alter_order_valid = True
+        elif(alter_order == "Filling"):
+            Gobal_filling = input_filling()
+            alter_order_valid = True
+        elif(alter_order == "Salads"):
+            Gobal_salad = input_salad()
+            alter_order_valid = True
+        else:
+            print("Please input a valid order to alter")
+    
 while(True):
     print("Enter O to order or enter E to end the day")
     End_day = input()
@@ -74,28 +98,12 @@ while(True):
         Gobal_Baguettes_bread = input_Baguettes_bread()
         Gobal_filling = input_filling()
         Gobal_salad = input_salad()
+        confirm_order_valid = False
         print("Enter Y to confirm the order or enter C to alter your order or enter X to discard your order")
         confirm_order = input()
         while(True):
             if(confirm_order == "C"):
-                while(True):
-                    print("Please type which order you wanted to alter")
-                    print("Type Baguettes Size,Baguettes Bread,Filling or Salads")
-                    alter_order = input()
-                    if(alter_order == "Baguettes Size"):
-                        Gobal_Baguettes_size = input_Baguettes_size()
-                        break
-                    elif(alter_order == "Baguettes Bread"):
-                        Gobal_Baguettes_bread = input_Baguettes_bread()
-                        break
-                    elif(alter_order == "Filling"):
-                        Gobal_filling = input_filling()
-                        break
-                    elif(alter_order == "Salads"):
-                        Gobal_salad = input_salad()
-                        break
-                    else:
-                        print("Please input a valid order to alter")
+                alter()
             if(confirm_order == "Y"):
                 count = count + 1
                 print("Your order number:")
